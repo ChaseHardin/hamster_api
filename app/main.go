5845b930c3b850edd0989426170d43856c628d72
Chase Hardin <chasehardin@Chases-MacBook-Pro.local>
2017-08-04 14:37:54 -0500

    Remove controller call.

--- app/main.go
+++ app/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/gorilla/mux"
-	"hamster_api/app/features/helloWorld"
 	"log"
 	"net/http"
 )
@@ -10,7 +9,7 @@ import (
 func main () {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/greet", helloWorld.GreetingHandler).Methods("GET")
+	//router.HandleFunc("/greet", helloWorld.GreetingHandler).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":4020", router))
 }
