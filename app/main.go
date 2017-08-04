bbd3353625aef82e19d39630f299825406dbce28
Chase Hardin <chasehardin@Chases-MacBook-Pro.local>
2017-08-04 11:03:09 -0500

    Add hello world endpoint.

--- app/main.go
+++ app/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"github.com/gorilla/mux"
+	"hamster_api/app/features/hello_world"
+	"log"
+	"net/http"
+)
+
 func main () {
-	print("Hello World!")
+	router := mux.NewRouter()
+
+	router.HandleFunc("/greet", hello_world.GreetingHandler).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":4020", router))
 }
