30982083cfcf7c0fd091f27f8cb7b114543f85af
Chase Hardin <chasehardin@Chases-MacBook-Pro.local>
2017-08-04 14:48:02 -0500

    Update project naming.

--- app/main.go
+++ app/main.go
@@ -4,12 +4,13 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"hamsterApi/app/features/helloWorld"
 )
 
 func main () {
 	router := mux.NewRouter()
 
-	//router.HandleFunc("/greet", helloWorld.GreetingHandler).Methods("GET")
+	router.HandleFunc("/greet", helloWorld.GreetingHandler).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":4020", router))
 }
